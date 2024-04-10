1c1422538e627f9a39ffa1ca56c23b3ff8b862e0
Flor Margarita Soto Higuera <101524283+munflowah@users.noreply.github.com>
2024-04-10 13:05:36 -0700

    cambio de main activity, se agregó if de string

--- app/src/main/java/com/example/practica11/MainActivity.kt
+++ app/src/main/java/com/example/practica11/MainActivity.kt
@@ -5,53 +5,58 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import com.example.practica11.R
+import com.example.practica11.User
 import com.google.firebase.database.ChildEventListener
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
-import org.w3c.dom.Text
-import kotlin.time.TimeMark
+import com.google.firebase.database.getValue
 
 class MainActivity : AppCompatActivity() {
-    private val userRef = FirebaseDatabase.getInstance().getReference("Users")
-
+    private val userRef=FirebaseDatabase.getInstance().getReference("Users")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var btnSave: Button = findViewById(R.id.save_button) as Button
+        var btnSave:Button=findViewById(R.id.save_button) as Button
         btnSave.setOnClickListener { saveMarkFromForm() }
 
-        userRef.addChildEventListener(object : ChildEventListener{
-            override fun onCancelled(error: DatabaseError){}
-            override fun onChildMoved(dataSnapshot: DataSnapshot, previousChildName: String?) {}
-            override fun onChildChanged(dataSnapshot: DataSnapshot, previousChildName: String?) {}
-            override fun onChildRemoved(dataSnapshot: DataSnapshot) {}
+        userRef.addChildEventListener(object: ChildEventListener{
+            override fun onCancelled(error: DatabaseError) {}
+            override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {}
+            override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {}
+            override fun onChildRemoved(snapshot: DataSnapshot) {}
 
             override fun onChildAdded(dataSnapshot: DataSnapshot, p1: String?) {
-                val usuario = dataSnapshot.getValue(User::class.java)
-                if(usuario != null) writeMark(usuario)
+                val value = dataSnapshot.getValue()
+
+                if(value is String){
+
+                }else if(value is User){
+                    val usuario = value
+                    if(usuario!=null){writeMark(usuario)}
+                }
             }
         })
-
     }
-    private fun saveMarkFromForm(){
-        var name: EditText=findViewById(R.id.et_name) as EditText
-        var lastName: EditText=findViewById(R.id.et_lastName) as EditText
-        var age: EditText=findViewById(R.id.et_age) as EditText
 
-        val usuario = User(
+    private fun saveMarkFromForm(){
+        var name:EditText=findViewById(R.id.et_name) as EditText
+        var lastName:EditText=findViewById(R.id.et_lastName) as EditText
+        var age:EditText=findViewById(R.id.et_age) as EditText
+
+        val usuario=User(
             name.text.toString(),
             lastName.text.toString(),
             age.text.toString()
-
         )
         userRef.push().setValue(usuario)
     }
 
     private fun writeMark(mark: User){
         var listV:TextView=findViewById(R.id.list_textView) as TextView
-        val text = listV.text.toString() + mark.toString() + "\n"
-        listV.text = text
+        val text=listV.text.toString()+ mark.toString()+ "\n"
+        listV.text=text
     }
 }
--- app/src/main/java/com/example/practica11/User.kt
+++ app/src/main/java/com/example/practica11/User.kt
@@ -1,8 +1,7 @@
 package com.example.practica11
 
-data class User(var firstName : String ?= null,
-                var lastName : String ?= null,
-                var age : String ?= null) {
-    override fun toString() = firstName + "\t" + lastName + "\t" + age
-
+data class User(var firstName:String?=null,
+                var lastName:String?=null,
+                var age:String?=null){
+    override fun toString()=firstName+"\t"+lastName+"\t"+age
 }
